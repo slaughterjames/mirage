43ddf146f06478212f52663015fc0717997497f2
slaughterjames <slaughter.james@gmail.com>
2017-10-22 18:41:33 +0100

    Mirage 0.5 initial push

--- get-mirage.sh
+++ get-mirage.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -
 #===============================================================================
-#Mirage v0.4 - Copyright 2017 James Slaughter,
-#This file is part of Mirage v0.4.
+#Mirage v0.5 - Copyright 2017 James Slaughter,
+#This file is part of Mirage v0.5.
 
-#Mirage v0.4 is free software: you can redistribute it and/or modify
+#Mirage v0.5 is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
 
-#Mirage v0.4 is distributed in the hope that it will be useful,
+#Mirage v0.5 is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
 
 #You should have received a copy of the GNU General Public License
-#along with Mirage v0.4.  If not, see <http://www.gnu.org/licenses/>.
+#along with Mirage v0.5.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 #------------------------------------------------------------------------------
 #
@@ -22,7 +22,7 @@
 #
 #------------------------------------------------------------------------------
 
-__ScriptVersion="Mirage-v0.4-1"
+__ScriptVersion="Mirage-v0.5-1"
 LOGFILE="/var/log/mirage-install.log"
 DIR="/opt/mirage/"
 MODULES_DIR="/opt/mirage/modules/"
