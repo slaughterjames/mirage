c4be2d64751eb9748ae548132784947ba1d24a95
James Slaughter <slaughter.james@gmail.com>
2024-05-02 11:32:12 +0100

    Update updatefeeds.sh

--- updatefeeds.sh
+++ updatefeeds.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -
 #===============================================================================
-#Mirage v0.9 - Copyright 2024 James Slaughter,
-#This file is part of Mirage v0.9.
+#Mirage v1.0 - Copyright 2024 James Slaughter,
+#This file is part of Mirage v1.0.
 
-#Mirage v0.9 is free software: you can redistribute it and/or modify
+#Mirage v1.0 is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
 
-#Mirage v0.9 is distributed in the hope that it will be useful,
+#Mirage v1.0 is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
 
 #You should have received a copy of the GNU General Public License
-#along with Mirage v0.9.  If not, see <http://www.gnu.org/licenses/>.
+#along with Mirage v1.0.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 #------------------------------------------------------------------------------
 #
